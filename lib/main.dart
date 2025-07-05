0926a9b13fe12e63f05c57a33faed11dbe520153
JustPlayHosting <pratyushpaswan312012@gmail.com>
2025-07-05 07:38:58 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,26 @@
+import 'package:flutter/material.dart';
+import 'screens/home/home_screen.dart';
+
 void main() {
-  print('Welcome to ARTIST - Video Editor');
+  runApp(ArtistApp());
+}
+
+class ArtistApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ARTIST - Video Editor',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: Colors.black,
+        fontFamily: 'Inter',
+        colorScheme: ColorScheme.dark(
+          primary: Colors.tealAccent,
+          secondary: Colors.teal,
+        ),
+      ),
+      home: HomeScreen(),
+    );
+  }
 }
